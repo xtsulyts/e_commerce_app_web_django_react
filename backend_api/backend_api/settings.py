43f9a4adaf5157e1fed367f66c8b8691b352f873
xtsulyts <531xtsulyts@gmail.com>
2025-04-27 00:34:19 -0300

    Creo directorio para frontend con NextJs.

--- backend_api/backend_api/settings.py
+++ backend_api/backend_api/settings.py
@@ -9,9 +9,15 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+#llamo a dot.env    
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,18 +132,15 @@ WSGI_APPLICATION = 'backend_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "e_commerce_shop_db",
-        "USER": "postgres",
-        "PASSWORD": "avmayo852",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- backend_api/core/urls.py
+++ backend_api/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.documentation import include_docs_urls
+from django.contrib import admin
 #from rest_framework.schemas import get_schema_vie
 from .views import (
     RegisterAPIView,
@@ -20,16 +21,10 @@ router.register(r'inventory', InventoryViewSet, basename='inventory')
 urlpatterns = [
     path('api/', include(router.urls)),
     # Tus otras URLs personalizadas aquí (ej: usuarios)
+    #path('admin', admin.site.urls),
     path('register/', RegisterAPIView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('profile/', ProfileAPIView.as_view(), name='profile'),
-    path('docs/', include_docs_urls(title='E-commerce Shop API')),
-    # path('docs/', include_docs_urls(
-    #     title='Documentación API',
-    #     public=True,
-    #     schema_url='/'
-    # )),
-    
-    #Schema CoreJSON (opcional)
-    #path('schema/', get_schema_view),
+    path('', include_docs_urls(title='E-commerce Shop API')),
+
 ]
